77b46b0b1b51f472d8b446be0eb518dadaa8d426
edcalderon <edwardca12@hotmail.com>
2021-03-30 18:51:40 -0500

    first commit

---
+++ internal/binary_keyset_writer.ts
@@ -0,0 +1,24 @@
+/**
+ * @license
+ * Copyright 2020 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import {SecurityException} from '../exception/security_exception';
+import {KeysetWriter} from './keyset_writer';
+import {PbEncryptedKeyset, PbKeyset} from './proto';
+
+/**
+ * KeysetWriter knows how to write a keyset or an encrypted keyset.
+ *
+ * @final
+ */
+export class BinaryKeysetWriter implements KeysetWriter {
+  /** @override */
+  write(keyset: PbKeyset|PbEncryptedKeyset): Uint8Array {
+    if (!keyset) {
+      throw new SecurityException('keyset has to be non-null.');
+    }
+    return keyset.serializeBinary();
+  }
+}
